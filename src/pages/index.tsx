51980defadce5ca7447413d4b86c056cd2c10d77
Andrew Brown <andrewbrown.n2000@gmail.com>
2025-03-11 15:51:10 -0400

    still fixing amplify deploy issues

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import Image from "next/image";
 import HomeButton from "../components/HomeButton";
 import { NavButton } from "../components/NavButton";
-import ScrollArrow from '../components/ScrollArrow';
+import ScrollArrow from "../components/ScrollArrow";
 
 export default function Home() {
 
@@ -46,7 +46,7 @@ export default function Home() {
               <Image src="/nycskyline_pfp.jpg" alt="NYC Skyline" width={500} height={500} />
             </div>
             <div className="flex flex-col justify-center items-center lg:order-1 w-full px-4 sm:px-0">
-              <h1 className="text-3xl sm:text-3xl lg:text-5xl text-center w-full">Hi! I'm <span className="text-pink-500 font-bold">Andrew Brown</span></h1>
+              <h1 className="text-3xl sm:text-3xl lg:text-5xl text-center w-full">Hi! I&apos;m <span className="text-pink-500 font-bold">Andrew Brown</span></h1>
               <p className="text-center w-full italic pt-2 mx-2 sm:mx-0 lg:text-lg">cloud architect, developer, skateboarder, gamer, and occasional baker</p>
             </div>
           </div> {/* Closing the grid container */}
@@ -64,7 +64,7 @@ export default function Home() {
               family computer, which led me to study <span className="text-pink-500">MIS</span> at <span className="text-pink-500">Oklahoma State University</span> and
               start my career in <span className="text-pink-500">IT Consulting</span>. After starting my career in
               IT consulting in Plano, I discovered cloud computing and eventually made the leap to <span className="text-pink-500">NYC</span> to expand my opportunities.
-              In my free time, I build personal projects—mostly in the cloud. Check out the projects section to see what I’ve been working on!</p>
+              In my free time, I build personal projects&mdash;mostly in the cloud. Check out the projects section to see what I&apos;ve been working on!</p>
             <p className="text-center max-w-4xl mx-auto p-4">I built this site as an opportunity to
               learn Next.js and AWS static website hosting, and to showcase my projects. Please reach out to me for any reason, my contacts are below!</p>
           </div>
